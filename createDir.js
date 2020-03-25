975a1ff5f70eaf3e7aa02630a98b47259d67e1fe
Ilya Trenin <itrenin@gmail.com>
2020-03-25 15:22:05 +0300

    bugfix: создается папка с результатом копирования

--- createDir.js
+++ createDir.js
@@ -3,10 +3,20 @@ const path = require('path')
 const copyFile = require('./copyFile')
 
 const createDir = (fileName, sourcePath, destinationPath) => {
-  const localDestPath = path.join(destinationPath.toString(), fileName[0].toUpperCase())
+  const localDestPath = path.join(
+    destinationPath.toString(),
+    fileName[0].toUpperCase()
+  )
   // console.log(localDestPath)
+  if (!fs.existsSync(destinationPath)) {
+    fs.mkdir(destinationPath, (err) => {
+      if (err) throw err
+    })
+  }
   if (!fs.existsSync(localDestPath)) {
-    fs.mkdirSync(localDestPath)
+    fs.mkdir(localDestPath, (err) => {
+      if (err) throw err
+    })
   }
   let destinationBase = path.join(localDestPath, fileName)
   // console.log(destinationBase)
