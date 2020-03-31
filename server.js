9f2ca608d019c4c73abffad664552c65ccac4142
Ilya Trenin <itrenin@gmail.com>
2020-03-31 23:48:43 +0300

    bugfix: исправлен вывод даты в браузер по таймауту

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-process.env.timeout = 10000
-process.env.timer = 1000
+// process.env.timeout = 10000
+// process.env.timer = 1000
 
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Получил ПОСТ')
-
-  printDateOnInterval(process.env.timer, process.env.timeout)
+  printDateOnInterval(res, process.env.timer, process.env.timeout)
 })
 
 app.listen(3000, () => {
   console.log('listening 3000')
 })
 
-function printDateOnInterval(timer, timeOut) {
+function printDateOnInterval(response, timer, timeOut) {
   const timerId = setInterval(() => {
     const now = new Date()
-    console.log(now.toLocaleString('ru'))
+    const end = new Date(Number(timeOut) + Number(now))
+
+    console.log(now.toUTCString())
     setTimeout(() => {
       clearInterval(timerId)
+      response.end(end.toUTCString())
     }, timeOut)
   }, timer)
 }
